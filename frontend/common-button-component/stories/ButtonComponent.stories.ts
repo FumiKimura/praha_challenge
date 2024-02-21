0fdae6b6873730961a755f04e4275426552a51ab
Norifumi Kimura <norical496@gmail.com>
2024-02-21 19:33:03 +0900

    fix 課題1と課題2の解答を追加

---
+++ frontend/common-button-component/stories/ButtonComponent.stories.ts
@@ -0,0 +1,39 @@
+import { ButtonComponent } from "./ButtonComponent";
+
+export default {
+  title: "Button",
+  component: ButtonComponent,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: { onClick: { action: "clicked" } },
+  tags: [],
+  addons: ["@storybook/addon-actions"],
+};
+
+export const Apply = {
+  args: {
+    children: "応募する",
+    color: "blue",
+    size: "medium",
+    disabled: false,
+  },
+};
+
+export const Delete = {
+  args: {
+    children: "削除する",
+    color: "red",
+    size: "small",
+    disabled: false,
+  },
+};
+
+export const Disabled = {
+  args: {
+    children: "削除する",
+    color: "red",
+    size: "small",
+    disabled: true,
+  },
+};
