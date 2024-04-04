60fb460b6a088f2efce93cf1194135cc443846d0
Norifumi Kimura <norical496@gmail.com>
2024-04-04 19:52:25 +0900

    課題2の解答を追加

--- frontend/understand-ref/src/App.tsx
+++ frontend/understand-ref/src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./App.css";
-import Compare from "./Compare";
+import Profiler from "./Profiler";
 
 function App() {
   return (
     <div className="App">
-      <Compare />
+      <Profiler />
     </div>
   );
 }
---
+++ frontend/understand-ref/src/Component.tsx
@@ -0,0 +1,5 @@
+function SomeComponent() {
+  return <p>Hello</p>;
+}
+
+export default SomeComponent;
