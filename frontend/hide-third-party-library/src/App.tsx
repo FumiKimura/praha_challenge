274d7f30101f178b2044e47e835a8f4b6c84c057
Norifumi Kimura <norical496@gmail.com>
2024-05-03 21:08:03 +0900

    fix: react-hook-formを分離

--- frontend/hide-third-party-library/src/App.tsx
+++ frontend/hide-third-party-library/src/App.tsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form';
-import Form from './Form';
+import { Form, selectProps, inputProp } from './Form';
 
 function App() {
   const {
@@ -10,12 +10,83 @@ function App() {
   const onSubmit = (data: any) => console.log(data);
   console.log(errors);
 
+  const inputs: inputProp[] = [
+    {
+      type: 'text',
+      placeholder: 'First name',
+      attributes: {
+        ...register('First name', { required: true, maxLength: 80 }),
+      },
+    },
+    {
+      type: 'text',
+      placeholder: 'Last name',
+      attributes: {
+        ...register('Last name', { required: true, maxLength: 100 }),
+      },
+    },
+    {
+      type: 'text',
+      placeholder: 'Email',
+      attributes: {
+        ...register('Email', { required: true, pattern: /^\S+@\S+$/i }),
+      },
+    },
+    {
+      type: 'tel',
+      placeholder: 'Mobile number',
+      attributes: {
+        ...register('Mobile number', {
+          required: true,
+          minLength: 6,
+          maxLength: 12,
+        }),
+      },
+    },
+    {
+      type: 'radio',
+      value: 'Yes',
+      attributes: {
+        ...register('Developer', { required: true }),
+      },
+    },
+    {
+      type: 'radio',
+      value: 'No',
+      attributes: {
+        ...register('Developer', { required: true }),
+      },
+    },
+  ];
+
+  const select: selectProps = {
+    attributes: { ...register('Title', { required: true }) },
+    options: [
+      {
+        value: 'Mr',
+        displayValue: 'Mr',
+      },
+      {
+        value: 'Mrs',
+        displayValue: 'Mrs',
+      },
+      {
+        value: 'Miss',
+        displayValue: 'Miss',
+      },
+      {
+        value: 'Dr',
+        displayValue: 'Dr',
+      },
+    ],
+  };
+
   return (
     <div>
       <Form
-        register={register}
-        handleSubmit={handleSubmit}
-        onSubmit={onSubmit}
+        inputs={inputs}
+        handleSubmit={handleSubmit(onSubmit)}
+        select={select}
       />
     </div>
   );
--- frontend/hide-third-party-library/src/Form.tsx
+++ frontend/hide-third-party-library/src/Form.tsx
@@ -1,64 +1,66 @@
 import {
-  FieldValues,
-  UseFormHandleSubmit,
-  UseFormRegister,
-} from 'react-hook-form';
+  DetailedHTMLProps,
+  FormEventHandler,
+  InputHTMLAttributes,
+  SelectHTMLAttributes,
+} from 'react';
 
-export interface FormProp {
-  readonly handleSubmit: UseFormHandleSubmit<FieldValues, undefined>;
-  readonly onSubmit: (data: any) => void;
-  readonly register: UseFormRegister<FieldValues>;
-}
+export type selectOption = {
+  readonly value: string;
+  readonly displayValue: string;
+};
 
-function Form(props: FormProp) {
-  const { handleSubmit, onSubmit, register } = props;
+export type selectProps = {
+  readonly attributes: DetailedHTMLProps<
+    SelectHTMLAttributes<HTMLSelectElement>,
+    HTMLSelectElement
+  >;
+  readonly options: selectOption[];
+};
+
+export type inputProp = {
+  readonly type: string;
+  readonly value?: string;
+  readonly placeholder?: string;
+  readonly attributes: InputHTMLAttributes<HTMLInputElement>;
+};
+
+export type FormProps = {
+  readonly handleSubmit?: FormEventHandler<HTMLFormElement>;
+  readonly inputs: inputProp[];
+  readonly select: selectProps;
+};
+
+export function Form(props: FormProps) {
+  const { handleSubmit, inputs, select } = props;
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        placeholder="First name"
-        {...register('First name', { required: true, maxLength: 80 })}
-      />
-      <input
-        type="text"
-        placeholder="Last name"
-        {...register('Last name', { required: true, maxLength: 100 })}
-      />
-      <input
-        type="text"
-        placeholder="Email"
-        {...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
-      />
-      <input
-        type="tel"
-        placeholder="Mobile number"
-        {...register('Mobile number', {
-          required: true,
-          minLength: 6,
-          maxLength: 12,
-        })}
-      />
-      <select {...register('Title', { required: true })}>
-        <option value="Mr">Mr</option>
-        <option value="Mrs">Mrs</option>
-        <option value="Miss">Miss</option>
-        <option value="Dr">Dr</option>
+    <form onSubmit={handleSubmit}>
+      {inputs.slice(0, 4).map((input) => (
+        <input
+          key={input.placeholder}
+          type={input.type}
+          placeholder={input.placeholder}
+          {...input.attributes}
+        ></input>
+      ))}
+
+      <select {...select.attributes}>
+        {select.options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.displayValue}
+          </option>
+        ))}
       </select>
 
-      <input
-        {...register('Developer', { required: true })}
-        type="radio"
-        value="Yes"
-      />
-      <input
-        {...register('Developer', { required: true })}
-        type="radio"
-        value="No"
-      />
+      {inputs.slice(4).map((input) => (
+        <input
+          key={input.value}
+          type={input.type}
+          {...input.attributes}
+        ></input>
+      ))}
 
       <input type="submit" />
     </form>
   );
 }
-
-export default Form;
