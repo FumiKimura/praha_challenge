9315ba3131e393c5029d9fe97a26bc7e5c40b8ab
Norifumi Kimura <norical496@gmail.com>
2024-05-09 20:29:59 +0900

    課題2の解答を追加

---
+++ frontend/understand-swr-and-react-query/src/fetch-component.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
+const getStars = async () => {
+  const response = await axios.get(
+    'https://api.github.com/repos/facebook/react'
+  );
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  return response.data.stargazers_count ?? 'データがありませんでした';
+};
+
+export const FetchComponent = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['getStars'],
+    queryFn: getStars,
+  });
+
+  if (isLoading) return <div>downloading...</div>;
+  if (error) return <div>An error occurred: {error.message}</div>;
+
+  return (
+    <div>
+      <p>ここにReactのGitHubレポジトリに付いたスターの数を表示してみよう</p>
+      <p>{data}個のスターでした。</p>
+    </div>
+  );
+};
