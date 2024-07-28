3c5486d0a8e54414c6ef758cc709531e33df7202
Norifumi Kimura <norical496@gmail.com>
2024-07-28 18:11:13 +0900

    課題2の解答を追加

--- テスト/storybookを作ろう/src/components/Board.jsx
+++ テスト/storybookを作ろう/src/components/Board.jsx
@@ -19,7 +19,7 @@ export function Board({xIsNext, squares, onPlay}) {
     if (winner) {
       status = "Winner: " + winner;
     } else {
-      status = "Next player: " + (xIsNext ? "X" : "O");
+      status = "次のプレーヤー: " + (xIsNext ? "X" : "O");
     }
   
     return (
